207e7b8d747e1adb2d848ebdc21a7c872873d718
Spenser Saling <spenser@brainiumstudios.com>
2020-09-03 08:37:50 -0700

    Add rust and update folder structure

---
+++ rust/hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+/Users/spenser/Documents/learning/rust/hello-rust/target/debug/deps/hello_rust: src/main.rs
+
+/Users/spenser/Documents/learning/rust/hello-rust/target/debug/deps/hello_rust.d: src/main.rs
+
+src/main.rs:
