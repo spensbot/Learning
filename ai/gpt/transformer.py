a7b56e8d65cac7d9baf90c62f6caaab9024a900a
Spenser Saling <spenser0saling@gmail.com>
2024-09-06 13:39:55 -0700

    Modify transformer.py so the output can be run in a Jupyter notebook

--- ai/gpt/transformer.py
+++ ai/gpt/transformer.py
@@ -11,34 +11,33 @@ import models
 timer.lap("Imports")
 
 BATCH_SIZE = 64
+LOSS_FN = nn.CrossEntropyLoss()
+DEVICE = util.pytorch_device('gpu')
 
-n_chars = len(data.chars2)
-device = util.pytorch_device('gpu')
-attention_params = models.MultiHeadAttentionParams(n_heads=4, n_qkv=64, masked=True)
-n_l = attention_params.n_l
-print(attention_params)
+def getModel(attention_params: models.MultiHeadAttentionParams, layers: int):
+    print(attention_params)
+    n_chars = len(data.chars2)
 
-transformer = models.Transformer(
-    n_tokens=n_chars, n_layers=2, attention_params=attention_params
-)
-transformer.to(device)
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(transformer.parameters(), lr=0.001)
+    transformer = models.Transformer(
+        n_tokens=n_chars, n_layers=layers, attention_params=attention_params
+    )
+    transformer.to(DEVICE)
+    return transformer
 
-losses = []
-
-
-def getBatch(data):
-    batch_indexes = torch.randint(0, len(data) - n_l, (BATCH_SIZE,))
-    X = torch.stack([data[i : i + n_l] for i in batch_indexes])
-    Y = torch.stack([data[i + 1 : i + n_l + 1] for i in batch_indexes])
-    return X.to(device), Y.to(device)
+def getBatch(dataSet, attention_params):
+    n_l = attention_params.n_l
+    batch_indexes = torch.randint(0, len(dataSet) - n_l, (BATCH_SIZE,))
+    X = torch.stack([dataSet[i : i + n_l] for i in batch_indexes])
+    Y = torch.stack([dataSet[i + 1 : i + n_l + 1] for i in batch_indexes])
+    return X.to(DEVICE), Y.to(DEVICE)
 
 
-def train(model, data, batch_count: int) -> None:
-    eval(model, testSet)
+def train(model, trainData, testData, batch_count: int) -> list[float]:
+    losses = []
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    eval(model, testData)
     for batch in range(batch_count):
-        X, Y = getBatch(data)
+        X, Y = getBatch(trainData, model.attention_params)
 
         # forward pass
         optimizer.zero_grad()
@@ -46,7 +45,7 @@ def train(model, data, batch_count: int) -> None:
         B, T, C = logits.shape
         logits = logits.view(B * T, C)
         targets = Y.view(B * T)
-        loss = loss_fn(logits, targets)
+        loss = LOSS_FN(logits, targets)
         losses.append(loss.item())
 
         # backward pass
@@ -55,29 +54,32 @@ def train(model, data, batch_count: int) -> None:
 
         if batch % 100 == 0:
             print(f"Batch {batch} Loss: {loss.item()}")
-            eval(model, testSet)
+            eval(model, testData)
+    return losses
 
 
 def eval(model, data) -> None:
     with torch.no_grad():
-        X, Y = getBatch(data)
+        X, Y = getBatch(data, model.attention_params)
 
         logits = model(X)  # (BATCH_SIZE, n_l, n_chars)
         B, T, C = logits.shape
         logits = logits.view(B * T, C)
         targets = Y.view(B * T)
-        loss = loss_fn(logits, targets)
-        losses.append(loss.item())
+        loss = LOSS_FN(logits, targets)
 
         print(f"Test Loss: {loss.item()}")
 
 
-def generate(model, len: int) -> str:
-    tokens = torch.zeros((1, n_l), dtype=torch.long).to(device)  # (B, L)
-    print(f"tokens.device: {tokens.device}")
+def generate(model, seed: str, count: int) -> str:
+    n_l = model.attention_params.n_l
+    n_pad = max(0, n_l - len(seed))
+    print(f"n_pad: {n_pad}")
+    tokens = data.encode(' ' * n_pad + '\n' + seed).unsqueeze(0).to(DEVICE) # (B, L)
+    # tokens = torch.zeros((1, n_l), dtype=torch.long).to(DEVICE)  # (B, L)
 
     with torch.no_grad():
-        for i in range(len):
+        for i in range(count):
             context = tokens[:, -n_l:]  # (1, n_l)
             logits = model(context)  # (1, n_l, n_chars)
             prediction_logits = logits[:, -1, :]  # (1, n_chars)
@@ -85,20 +87,23 @@ def generate(model, len: int) -> str:
             next_token = torch.multinomial(probs, 1)  # (1, 1)
             tokens = torch.cat([tokens, next_token], dim=1)  # (1, L)
 
-        return data.decode(tokens[0].tolist())
+        return data.decode(tokens[0].tolist())[n_pad:]
 
+if __name__ == "__main__":
+    model = getModel(models.MultiHeadAttentionParams(n_heads=4, n_qkv=64, masked=True), layers=2)
 
-print("\n\nBEFORE TRAINING\n\n")
-print(generate(transformer, 1000))
+    print("\n\nBEFORE TRAINING\n\n")
+    seed = "LEWIS: What is't thou doing, fool? I thought we would feast this day?\nKING GODDARD: "
+    print(generate(model, seed, 1000))
 
-trainSet, testSet = data.load(0.8)
-timer.lap("Loading")
-train(transformer, trainSet, 5000)
-timer.lap("Training")
+    trainSet, testSet = data.load(0.8)
+    timer.lap("Loading")
+    losses = train(model, trainSet, testSet, 1000)
+    timer.lap("Training")
 
-print("\n\nAFTER TRAINING\n\n")
-print(generate(transformer, 1000))
-timer.lap("Generate")
+    print("\n\nAFTER TRAINING\n\n")
+    print(generate(model, seed, 1000))
+    timer.lap("Generate")
 
-plt.plot(losses)
-plt.show()
+    plt.plot(losses)
+    plt.show()
